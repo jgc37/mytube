189781cd769a1338bd7d9e3074b589ab997d6ad0
jgc37 <46300206+jgc37@users.noreply.github.com>
2019-03-13 16:46:39 -0500

    added to subscriptions
    still waiting on a working youtube api :pensive:

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -2,102 +2,89 @@ import 'package:flutter/material.dart';
 import 'package:xml/xml.dart' as xml;
 import 'package:flutter/cupertino.dart';
 import 'subscriptions.dart';
-import 'trending.dart';
 
-class Home extends StatefulWidget{
+class Home extends StatefulWidget {
   HomeState createState() => new HomeState();
 }
 
-
 class HomeState extends State<Home> with SingleTickerProviderStateMixin {
-
   TabController tcontrol;
   String title;
 
-
-    setTitle(){
-    if(tcontrol.index == 0){
-      setState(() {
-        title = "Trending";
-        
-      });
-    }else if(tcontrol.index == 1){
+  setTitle() {
+    if (tcontrol.index == 0) {
       setState(() {
         title = "Subscriptions";
-        
       });
-    }else if(tcontrol.index == 2){
+    } else if (tcontrol.index == 1) {
+      setState(() {
+        title = "Channels";
+      });
+    } else if (tcontrol.index == 2) {
       setState(() {
         title = "Settings";
-        
       });
     }
   }
 
-  @override void initState() { 
-  super.initState();
-     tcontrol = new TabController(vsync: this,length: 3);
-     tcontrol.addListener(setTitle);
-     setTitle();
+  @override
+  void initState() {
+    super.initState();
+    tcontrol = new TabController(vsync: this, length: 3);
+    tcontrol.addListener(setTitle);
+    setTitle();
   }
 
-  
   @override
- void dispose() {
-   tcontrol.dispose();
-   super.dispose();
-   
- }
+  void dispose() {
+    tcontrol.dispose();
+    super.dispose();
+  }
 
-
-
-  
   @override
   Widget build(BuildContext context) {
-    
-    
-    return  new Scaffold(
-        appBar: new AppBar(
-          title: new Text(title),
-          backgroundColor: Colors.red[600],
-          bottom: new TabBar(
-            controller: tcontrol,
-            isScrollable: true,
-            indicatorColor: Colors.white,
-            indicatorPadding:EdgeInsets.fromLTRB(50, 0, 50, 0) ,
-            labelPadding: EdgeInsets.fromLTRB(50, 0, 50, 0),
-            indicatorSize: TabBarIndicatorSize.label,
-            tabs: <Widget>[
-              new Tab(
-                icon: new Icon(Icons.whatshot),
-              ),
-              new Tab(
-                icon: new Icon(Icons.subscriptions),
-              ),
-              new Tab(
-                icon: new Icon(Icons.settings), 
-              )
-            ],
-          ),
-        ),
-        body: new TabBarView(
-          controller:  tcontrol,
-          children: <Widget>[
-            new trending(),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
+        backgroundColor: Colors.red[600],
+        bottom: new TabBar(
+          controller: tcontrol,
+          isScrollable: true,
+          indicatorColor: Colors.white,
+          labelPadding: EdgeInsets.fromLTRB(50, 0, 50, 0),
+          indicatorPadding: EdgeInsets.fromLTRB(50,0,50,0),
+          indicatorSize: TabBarIndicatorSize.label,
+          tabs: <Widget>[
             new Tab(
-              child: new Center(
-                child: new Text("Subscriptions"),
-              ),
+              icon: new Icon(Icons.subscriptions),
             ),
             new Tab(
-              child: new Center(
-                child: new Text("Settings"),
-              ),
+              icon: new Icon(Icons.people),
             ),
+            new Tab(
+              icon: new Icon(Icons.settings),
+            )
           ],
         ),
-        backgroundColor: Colors.grey[800],
-      );
+      ),
+      body: new TabBarView(
+        controller: tcontrol,
+        children: <Widget>[
+          new Subscriptions(),
+          new Tab(
+            child: new Center(
+              child: new Text("Channels"),
+            ),
+          ),
+          new Tab(
+            child: new Center(
+              child: new Text("Settings"),
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: Colors.grey[800],
+    );
   }
+ 
 }
-
