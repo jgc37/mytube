aad0aafac47043af73a74124ba8f42d616da8c6d
jgc37 <46300206+jgc37@users.noreply.github.com>
2019-03-15 13:16:59 -0500

    Update channels.dart

--- lib/ui/channels.dart
+++ lib/ui/channels.dart
@@ -6,7 +6,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:html/parser.dart';
 
-Future<ChannelInfo> getChannelsInfo(MapEntry m) async {
+Future<getChannelInfo> getChannelsInfo(MapEntry m) async {
   String url = m.value;
   http.Response response = await http.get(url);
   xml.XmlDocument xtxt = xml.parse(response.body);
@@ -20,18 +20,18 @@ Future<ChannelInfo> getChannelsInfo(MapEntry m) async {
   var rawUrl =
       hurl.body.replaceAll("\n", " ").replaceAll(new RegExp(r"\s+\b|\b\s"), "");
   String Url = rawUrl.substring(86, rawUrl.length - 65);
-  return ChannelInfo.fromxml(xtxt, Url);
+  return getChannelInfo.fromxml(xtxt, Url);
 }
 
-class ChannelInfo {
+class getChannelInfo {
   final String ChannelName;
   final String ChannelUrl;
   final String ChannelProfileUrl;
 
-  ChannelInfo({this.ChannelName, this.ChannelUrl, this.ChannelProfileUrl});
+  getChannelInfo({this.ChannelName, this.ChannelUrl, this.ChannelProfileUrl});
 
-  factory ChannelInfo.fromxml(xml.XmlDocument cXml, String jsonData) {
-    return ChannelInfo(
+  factory getChannelInfo.fromxml(xml.XmlDocument cXml, String jsonData) {
+    return getChannelInfo(
       ChannelName:
           cXml.findAllElements("title").map((node) => node.text).toList()[0],
       ChannelUrl:
@@ -40,33 +40,85 @@ class ChannelInfo {
     );
   }
 }
+class ChannelInfo{
+
+   static int ChannelNumber;
+   String channelUrl = getValues("ChannelUrl");
+   String channelProfileUrl = getValues("ChannelProfileUrl");
+   String subscriberCount;
+
+  ChannelInfo(int n){
+    ChannelNumber = n;
+  }
+
+  static String getValues(String value){
+    FutureBuilder(
+      future: getChannelsInfo(getChannels().entries.toList()[ChannelNumber]),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        return snapshot.data.value;
+      },
+    );
+  }
+}
 
 class Channels extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new ListView.builder(
-      itemCount: getChannels().length,
-      itemBuilder: (BuildContext context, int index) {
-        return new ListTile(
-          title: new Text(
-            getChannels().keys.toList()[index],
-            style: new TextStyle(color: Colors.white),
-          ),
-          leading: FutureBuilder(
-            future: getChannelsInfo(getChannels().entries.toList()[index]),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return new CircleAvatar(
-                  backgroundImage:
-                      new NetworkImage(snapshot.data.ChannelProfileUrl),
-                );
-              } else {
-                return new CircularProgressIndicator();
-              }
-            },
-          ),
-        );
-      },
+    return new InkWell(
+      child: new ListView.builder(
+          itemCount: getChannels().length,
+          itemBuilder: (BuildContext context, int index) {
+            return new ListTile(
+              title: new Text(
+                getChannels().keys.toList()[index],
+                style: new TextStyle(color: Colors.white),
+              ),
+              leading: new CircleAvatar(
+                      backgroundImage:
+                          new NetworkImage(ChannelInfo(index).channelProfileUrl),
+                    ),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (context) => new ChannelPage(index)));
+              },
+            );
+          }),
+    );
+  }
+}
+
+class ChannelPage extends StatelessWidget {
+  int pageNumber;
+  ChannelPage(int p) {
+    this.pageNumber = p;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.grey[800],
+      appBar: new AppBar(
+        title: new Text(getChannels().keys.toList()[pageNumber]),
+        backgroundColor: Colors.red[600],
+      ),
+      body: new Column(
+        children: <Widget>[
+          new Card(
+            color: Colors.grey[700],
+            child: new Row(
+              children: <Widget>[
+                new CircleAvatar(
+                        radius: 40,
+                        backgroundImage:
+                            new NetworkImage(ChannelInfo(pageNumber).channelProfileUrl),
+                      ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
